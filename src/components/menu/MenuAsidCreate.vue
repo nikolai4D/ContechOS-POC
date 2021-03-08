cb99191af989bccbcb559d5f8357d69005369035
Maja Frankiewicz <majafrank@live.se>
2021-03-08 10:56:49 +0100

    Update clicking rel in create node rel shows message in sidebar

--- src/components/menu/MenuAsidCreate.vue
+++ src/components/menu/MenuAsidCreate.vue
@@ -13,73 +13,78 @@
             <br />
             <MenuCardNodeActive />
           </div>
-          <div v-if="objCreate.type == 'create rel' && secondAct == null">
-            <MenuCardNodeNotActive :secondAct="secondAct" />
+          <div v-if="activeObj.source">
+            <v-list-item><h5>Välj en nod (den valda är en relation)</h5></v-list-item>
           </div>
-          <div v-else-if="objCreate.type == 'create rel' && secondAct != null">
-            <MenuCardAsidRel :relList="setConfigConfig" :secondAct="secondAct"/>
-            <MenuCardNodeSecondActive :secondAct="secondAct" />
-          </div>
+          <div v-else>
+            <div v-if="objCreate.type == 'create rel' && secondAct == null">
+              <MenuCardNodeNotActive :secondAct="secondAct" />
+            </div>
+            <div v-else-if="objCreate.type == 'create rel' && secondAct != null">
+              <MenuCardAsidRel :relList="setConfigConfig" :secondAct="secondAct" />
+              <MenuCardNodeSecondActive :secondAct="secondAct" />
+            </div>
 
-          <div v-else-if="objCreate.type == 'create to' || objCreate.type == 'create from'">
-            <MenuCardAsidRel v-if="objCreate.type == 'create to'" :relList="setConfigConfig" />
-            <MenuCardAsidRel v-if="objCreate.type == 'create from'" :relList="setConfigConfig" />
+            <div v-else-if="objCreate.type == 'create to' || objCreate.type == 'create from'">
+              <MenuCardAsidRel v-if="objCreate.type == 'create to'" :relList="setConfigConfig" />
+              <MenuCardAsidRel v-if="objCreate.type == 'create from'" :relList="setConfigConfig" />
 
-            <v-sheet outlined dark rounded>
-              <v-list-item>
-                <v-list-item-subtitle>(Ny)</v-list-item-subtitle>
-              </v-list-item>
-              <MenuSidCreateNodeParentNode v-if="selectedGraph != 'Admin'" />
-              <MenuSystemCreateLabels v-if="selectedGraph != 'Admin'" />
-              <MenuAsidCreateNodeLabels v-if="selectedGraph == 'Admin'" />
-              <MenuAsidCreateNodeProps />
-              <MenuAsidCreateNodeChildProps />
-              <MenuSidCreateNodeParentRels
-                v-if="selectedGraph != 'Admin' && selectedGraph != 'System'"
-              />
-            </v-sheet>
-            <br />
-          </div>
-
-          <!-- Create new node only -->
-
-          <div v-else-if="objCreate.type=='create'">
-            <div>
-              <h3>{{createString.new}}</h3>
+              <v-sheet outlined dark rounded>
+                <v-list-item>
+                  <v-list-item-subtitle>(Ny)</v-list-item-subtitle>
+                </v-list-item>
+                <MenuSidCreateNodeParentNode v-if="selectedGraph != 'Admin'" />
+                <MenuSystemCreateLabels v-if="selectedGraph != 'Admin'" />
+                <MenuAsidCreateNodeLabels v-if="selectedGraph == 'Admin'" />
+                <MenuAsidCreateNodeProps />
+                <MenuAsidCreateNodeChildProps />
+                <MenuSidCreateNodeParentRels
+                  v-if="selectedGraph != 'Admin' && selectedGraph != 'System'"
+                />
+              </v-sheet>
               <br />
             </div>
-            <v-sheet outlined dark rounded>
-              <v-list-item>
-                <v-list-item-subtitle>(Ny)</v-list-item-subtitle>
-              </v-list-item>
-              <MenuSidCreateNodeParentNode v-if="selectedGraph != 'Admin'" />
-              <MenuSystemCreateLabels v-if="selectedGraph != 'Admin'" />
-              <MenuAsidCreateNodeLabels v-if="selectedGraph == 'Admin'" />
-              <MenuAsidCreateNodeProps />
-              <MenuAsidCreateShowRels v-if="selectedGraph != 'Admin'" />
-              <MenuAsidCreateNodeChildProps />
-            </v-sheet>
-          </div>
 
-          <v-list-item>
-            <v-list-item-action>
-              <v-btn
-                :loading="loading3"
-                @click="validate"
-                :color="button.color"
-                :disabled="button.disabled"
-                icon
-              >
-                <v-icon color="grey lighten-1">{{button.text}}</v-icon>
-              </v-btn>
-            </v-list-item-action>
-            <v-list-item-action>
-              <v-btn @click="cancel" v-if="successful=='...'" icon>
-                <v-icon color="grey lighten-1">mdi-close</v-icon>
-              </v-btn>
-            </v-list-item-action>
-            <v-list-item-action v-if="successful == true" color="green">{{button.successText}}</v-list-item-action>
-          </v-list-item>
+            <!-- Create new node only -->
+
+            <div v-else-if="objCreate.type=='create'">
+              <div>
+                <h3>{{createString.new}}</h3>
+                <br />
+              </div>
+              <v-sheet outlined dark rounded>
+                <v-list-item>
+                  <v-list-item-subtitle>(Ny)</v-list-item-subtitle>
+                </v-list-item>
+                <MenuSidCreateNodeParentNode v-if="selectedGraph != 'Admin'" />
+                <MenuSystemCreateLabels v-if="selectedGraph != 'Admin'" />
+                <MenuAsidCreateNodeLabels v-if="selectedGraph == 'Admin'" />
+                <MenuAsidCreateNodeProps />
+                <MenuAsidCreateShowRels v-if="selectedGraph != 'Admin'" />
+                <MenuAsidCreateNodeChildProps />
+              </v-sheet>
+            </div>
+
+            <v-list-item v-else>
+              <v-list-item-action>
+                <v-btn
+                  :loading="loading3"
+                  @click="validate"
+                  :color="button.color"
+                  :disabled="button.disabled"
+                  icon
+                >
+                  <v-icon color="grey lighten-1">{{button.text}}</v-icon>
+                </v-btn>
+              </v-list-item-action>
+              <v-list-item-action>
+                <v-btn @click="cancel" v-if="successful=='...'" icon>
+                  <v-icon color="grey lighten-1">mdi-close</v-icon>
+                </v-btn>
+              </v-list-item-action>
+              <v-list-item-action v-if="successful == true" color="green">{{button.successText}}</v-list-item-action>
+            </v-list-item>
+          </div>
         </v-form>
       </v-list>
     </v-card-text>
@@ -210,10 +215,13 @@ export default {
             // this.createObj.rel.type.value = this.createObj.rel.type.valueField;
           }
           // create rel
-          if (this.$store.state.selectedGraph != "Config" &&
-            this.$store.state.selectedGraph != "Admin" && this.$store.state.propsToAdd.parentRel == null){
-            console.error('No relationship chosen.')
-            return 0
+          if (
+            this.$store.state.selectedGraph != "Config" &&
+            this.$store.state.selectedGraph != "Admin" &&
+            this.$store.state.propsToAdd.parentRel == null
+          ) {
+            console.error("No relationship chosen.");
+            return 0;
           }
           this.objCreate.toggle = 1;
           this.createObj.rel.from = this.activeObj.id;
@@ -327,14 +335,13 @@ export default {
             ) {
               this.createObj.node["props"] = this.$store.state.propsToShow;
               if (this.$store.state.selectedGraph != "System") {
-    
-              const objectMap = (obj, fn) =>
-                Object.fromEntries(
-                  Object.entries(obj).map(([k, v], i) => [k, fn(v, k, i)])
-                );
-              this.createObj.relss = [];
-              objectMap(this.relsInfoData, v => this.createObj.relss.push(v));
-   }
+                const objectMap = (obj, fn) =>
+                  Object.fromEntries(
+                    Object.entries(obj).map(([k, v], i) => [k, fn(v, k, i)])
+                  );
+                this.createObj.relss = [];
+                objectMap(this.relsInfoData, v => this.createObj.relss.push(v));
+              }
               this.createObj[
                 "parentNodeId"
               ] = this.$store.state.propsToAdd.valueId;
@@ -364,7 +371,7 @@ export default {
               this.selectedGraph == "Information" ||
               this.selectedGraph == "Data"
             ) {
-                          this.$store.dispatch("createInfoDataSubNodeRel", this.createObj);
+              this.$store.dispatch("createInfoDataSubNodeRel", this.createObj);
 
               console.log("create from, info data", this.createObj);
             } else {
