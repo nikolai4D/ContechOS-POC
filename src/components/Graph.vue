08763dfb34315d02c52ca5d9193157a14ea54548
Maja Frankiewicz <majafrank@live.se>
2021-03-09 11:06:49 +0100

    Added self links

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -439,7 +439,7 @@ export default {
         .attr("markerWidth", 11)
         .attr("markerHeight", 11)
         .attr("orient", "auto")
-        .append("svg:path")
+        .append("path")
         .attr("d", "M 0,-5 L 10 ,0 L 0,5")
         .attr("fill", "#999")
         .attr("overflow", "visible");
@@ -454,15 +454,15 @@ export default {
             .forceLink()
             .id(d => d.id)
             .distance(200)
-          //  .gravity(.06)
         )
         .force("charge", d3.forceManyBody().strength(-2000))
-        // .force(
-        //   "center",
-        //   d3.forceCenter(width / 2 - width / 12, height / 2 - height / 8)
-        // )
-         .force('collide',d3.forceCollide().radius(60).iterations(2))
-
+        .force(
+          "collide",
+          d3
+            .forceCollide()
+            .radius(60)
+            .iterations(2)
+        )
         .force(
           "x",
           d3
@@ -481,21 +481,21 @@ export default {
 
       // Create links
 
-      const link = svg
-        .selectAll("line")
-        .data(this.graph.links)
-        .join(
-          enter =>
-            enter
-              .append("line")
-              .attr("id", function(d) {
-                return "edge" + d.id;
-              })
-              .attr("class", "linkSVG")
-              .attr("marker-end", "url(#end-arrow)"),
-          exit => exit.remove()
-        );
 
+      const link = svg
+        .append("g")
+        .selectAll("path")
+
+        .data(this.graph.links)
+        .enter()
+        .append("path")
+        .attr("id", function(d) {
+          return "edge" + d.id;
+        })
+        .attr("marker-end", d => {
+          return d.source == d.target ? "" : "url(#end-arrow)";
+        })
+        .attr("class", "linkSVG");
       // Create nodes
       // var that = this
       const node = svg
@@ -593,6 +593,62 @@ export default {
           .attr("x2", d => d.target.x)
           .attr("y2", d => d.target.y);
 
+        link.attr("d", function(d) {
+          var x1 = d.source.x,
+            y1 = d.source.y,
+            x2 = d.target.x,
+            y2 = d.target.y,
+            dr = 0,
+            // Defaults for normal edge.
+            drx = dr,
+            dry = dr,
+            xRotation = 0, // degrees
+            largeArc = 0, // 1 or 0
+            sweep = 1; // 1 or 0
+
+          // Self edge.
+          if (x1 === x2 && y1 === y2) {
+            // Fiddle with this angle to get loop oriented.
+            xRotation = -45;
+
+            // Needs to be 1.
+            largeArc = 1;
+
+            // Change sweep to change orientation of loop.
+            //sweep = 0;
+
+            // Make drx and dry different to get an ellipse
+            // instead of a circle.
+            drx = 40;
+            dry = 20;
+
+            // For whatever reason the arc collapses to a point if the beginning
+            // and ending points of the arc are the same, so kludge it.
+            x2 = x2 + 1;
+            y2 = y2 + 1;
+          }
+          return (
+            "M" +
+            x1 +
+            "," +
+            y1 +
+            "A" +
+            drx +
+            "," +
+            dry +
+            " " +
+            xRotation +
+            "," +
+            largeArc +
+            "," +
+            sweep +
+            " " +
+            x2 +
+            "," +
+            y2
+          );
+        });
+
         node.attr("cx", d => d.x).attr("cy", d => d.y);
         //          .attr("transform", function (d) {
         //        d.x = Math.max(this.margin.r, Math.min(width - this.margin.r, d.x));
@@ -613,11 +669,15 @@ export default {
           let rx = bbox.x + bbox.width / 2;
           let ry = bbox.y + bbox.height / 2;
           let theAngle = angle(d.source.x, d.source.y, d.target.x, d.target.y);
+                    if (d.target == d.source) {
+            return "rotate(" + 1 + " " + (rx+3500) + " " + (ry+2800) + ")"
+          }
           if (d.target.x < d.source.x) {
             return "rotate(" + (180 + theAngle) + " " + rx + " " + ry + ")";
           } else {
             return "rotate(" + theAngle + " " + rx + " " + ry + ")";
           }
+
         });
 
         nodeLabels
@@ -871,6 +931,7 @@ export default {
 .linkSVG {
   stroke: #999;
   stroke-opacity: 0.6;
+  fill: none;
 }
 
 .textEdge {
